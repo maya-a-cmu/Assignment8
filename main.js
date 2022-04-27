a3692c318abc6d8eb730293bf733399691edc9d4
maya-a-cmu <57306579+maya-a-cmu@users.noreply.github.com>
2022-04-27 14:45:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -42,27 +42,27 @@
 
 ////////////Publication Slide Shows //////////////////////
               function plusDivs(n) {
-                showDivs(slideIndex += n);
+                showDivs(slideNumber += n);
               }
               
               function currentDiv(n) {
-                showDivs(slideIndex = n);
+                showDivs(slideNumber = n);
               }
               
               function showDivs(n) {
                 var i;
                 var x = document.getElementsByClassName("mySlides");
                 var dots = document.getElementsByClassName("demo");
-                if (n > x.length) {slideIndex = 1}    
-                if (n < 1) {slideIndex = x.length}
+                if (n > x.length) {slideNumber = 1}    
+                if (n < 1) {slideNumber = x.length}
                 for (i = 0; i < x.length; i++) {
                   x[i].style.display = "none";  
                 }
                 for (i = 0; i < dots.length; i++) {
                   dots[i].className = dots[i].className.replace(" w3-red", "");
                 }
-                x[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " w3-red";
+                x[slideNumber-1].style.display = "block";  
+                dots[slideNumber-1].className += " w3-red";
               }
 //////////////////////////////////////////////////////////////////
 
